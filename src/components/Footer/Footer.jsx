be503f1cc8cb10dc5ac9c85f721ec2bc79974913
Kenneth Lowe <kennethlowe45@yahoo.COM>
2024-12-11 07:50:23 -0600

    fixed footer tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import "./Footer.css";
 function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="footer__content">
+    <footer className="footer__content">
       <div>
         <p className="footer__developer-text">Developed By kenneth Lowe</p>
       </div>
       <div>
         <p className="footer__year-text">{currentYear}</p>
       </div>
-    </div>
+    </footer>
   );
 }
 
