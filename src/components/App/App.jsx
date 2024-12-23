5dc01b1d622729d6119b77ebd8c83b25abd6dce8
Kenneth Lowe <kennethlowe45@yahoo.COM>
2024-12-22 18:31:45 -0600

    fixed/weatherApi

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -131,7 +131,7 @@ function App() {
                 <Profile
                   handleCardClick={handleCardClick}
                   clothingItems={clothingItems}
-                  onAddNewClick={() => setActiveModal("add-garment")}
+                  onAddNewClick={handleAddClick}
                 />
               }
             />
